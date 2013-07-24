a93e20aad370f1ee3b06851d2717697c24e08a0a
Thomas Heller <thom.heller@gmail.com>
2013-07-24 12:06:34 +0200

    more work on everything

---
+++ src/mainwindow.hpp
@@ -0,0 +1,35 @@
+//  Copyright (c) 2012-2013 Thomas Heller
+//  Copyright (c) 2012-2013 Andreas Schaefer
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef VANDOUKEN_MAINWINDOW_HPP
+#define VANDOUKEN_MAINWINDOW_HPP
+
+#include "ui_main.h"
+
+#include <libgeodecomp/misc/coord.h>
+
+#include <QMainWindow>
+#include <QTimer>
+
+namespace vandouken {
+    class GridProvider;
+    class MainWindow : public QMainWindow
+    {
+        Q_OBJECT;
+
+    public:
+        MainWindow(
+            LibGeoDecomp::Coord<2> dim,
+            GridProvider *gridProvider,
+            QWidget *parent=0);
+
+    private:
+        Ui_MainWindow content;
+        QTimer paintTimer;
+    };
+}
+
+#endif
